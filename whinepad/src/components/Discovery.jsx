995faa6c8033e5eea21791282d0d93c77e012104
nikmel238 <man.nikita0@yandex.ru>
2024-06-02 17:46:32 +0300

    Created LogoComponent (Whinepad)

--- whinepad/src/components/Discovery.jsx
+++ whinepad/src/components/Discovery.jsx
@@ -1,10 +1,12 @@
 import Excel from "./Excel";
+import Logo from "./Logo";
 
 function Discovery() {
     return (
         <div>
             <h2>Excel</h2>
             <Excel headers={[`Name`, `Year`]} initialData={[[`Charles`, `1859`], [`Antoine`, `1943`]]}/>
+            <Logo />
         </div>
     );
 }
---
+++ whinepad/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import logo from './../images/whinepad-logo.svg';
+
+const Logo = () => {
+    return (
+        <img src={logo} width={`300`} alt={`Whinepad logo`}/>
+    );
+}
+
+export default Logo;
