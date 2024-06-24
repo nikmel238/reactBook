87013f2fd00a3e6df144070598fbd5549628e2d6
nikmel238 <man.nikita0@yandex.ru>
2024-06-24 21:40:38 +0300

    Added Button  components in Whinepad

---
+++ whinepad/src/components/Button.jsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import './Button.css';
+
+const Button = (props) => {
+  return props.href ? (
+    <a {...props} className={classNames('Button', props.className)}>
+      {props.children}
+    </a>
+  ) : (
+    <button {...props} className={classNames('Button', props.className)} />
+  );
+}
+
+Button.propTypes = {
+  href: PropTypes.string,
+};
+
+export default Button;
--- whinepad/src/components/Discovery.jsx
+++ whinepad/src/components/Discovery.jsx
@@ -1,6 +1,7 @@
 import Body from "./Body";
 import Excel from "./Excel";
 import Logo from "./Logo";
+import Button from "./Button";
 
 function Discovery() {
     return (
@@ -10,7 +11,20 @@ function Discovery() {
             <h2>Logo</h2>
             <Logo />
             <h2>Body</h2>
-            <Body />
+            <Body>Тут будет контент</Body>
+            <h2>Кнопки</h2>
+            <p>
+                Кнопка с нажатием: {` `}
+                <Button onClick={()=>alert(`ouch`)}>Нажми на меня</Button>    
+            </p>
+            <p>
+                Ссылка: <Button href={`https://reactjs.org/`}>Перейти</Button>
+            </p>
+            <p>
+                Пользовательское имя класа: {` `}
+                <Button className={`Discovery-custom-button`}>Ничего не делаю</Button>
+            </p>
+
         </div>
     );
 }
